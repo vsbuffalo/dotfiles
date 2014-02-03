2f22dad3ad2f4e9eac3ced0c1d1554b21366d5e3
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2014-02-03 01:42:54 -0800

    refactored setup script

--- setup.sh
+++ setup.sh
@@ -8,111 +8,91 @@ Red='\033[0;31m'
 Yel='\033[0;33m'
 
 function gecho {
-    echo "${Gre}$1${RCol}"
+  echo "${Gre}[message] $1${RCol}"
 }
 
 function yecho {
-    echo "${Yel}$1${RCol}"
+  echo "${Yel}[warning] $1${RCol}"
 }
 
 function recho {
-    echo "${Red}$1${RCol}"
+  echo "${Red}[error] $1${RCol}"
+  exit 1
 }
 
-
 # Check for pre-requisites
-(command -v gcc > /dev/null  && gecho "[message] GCC found...") || (recho "[error] GCC not found, install via XCode." && exit 1)
-(command -v brew > /dev/null && gecho "[message] Homebew found...") || (recho "[error] Homebrew not found, install at http://brew.sh/" && exit 1)
-(command -v git > /dev/null && gecho "[message] Git found...") || (yecho "[message] Git not found, installing from Homebew" && brew install git)
-(command -v ag > /dev/null && gecho "[message] Ag found...") || (yecho "[message] Ag not found, installing from Homebew" && brew install the_silver_searcher)
+(command -v gcc > /dev/null  && gecho "GCC found...") || recho "GCC not found, install via XCode."
+(command -v brew > /dev/null && gecho "Homebrew found...") || recho "Homebrew not found, install at http://brew.sh/"
+(brew doctor > /dev/null && gecho "brew doctor looks good...") || recho "brew doctor returned with non-zero exit status. Run brew doctor and debug."
+
+# Install Homebrew main programs
+(command -v git > /dev/null && gecho "Git found...") || (yecho "Git not found, installing from Homebrew" && brew install git)
+(command -v ag > /dev/null && gecho "Ag found...") || (yecho "Ag not found, installing from Homebrew" && brew install the_silver_searcher)
+(command -v tmux > /dev/null && gecho "tmux found...") || (yecho "tmux not found, installing from Homebrew" && brew install tmux)
+(command -v vim > /dev/null && gecho "Vim found...") || (yecho "Vim not found, installing from Homebrew" && brew install vim)
+(command -v mvim > /dev/null && gecho "MacVim found...") || (yecho "MacVim not found, installing from Homebrew" && brew install macvim --env-std --override-system-vim)
+
+# function for linking dotfiles
+function linkdotfile {
+  file="$1"
+  if [ ! -e ~/$file -a ! -L ~/$file ]; then
+      yecho "$file not found, linking..." >&2
+      ln -s dotfiles/$file ~/$file
+  else
+      gecho "$file found, ignoring..." >&2
+  fi
+}
 
 # Download oh-my-zsh
 if [ ! -d ~/.oh-my-zsh ]; then
-    yecho "[message] ~/.oh-my-zsh not found, installing from Github..." >&2
+    yecho ".oh-my-zsh not found, installing from Github..." >&2
     (cd ~ && curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh 2> /dev/null | sh)
 else
-    gecho "[message] ~/.oh-my-zsh found, ignoring..." >&2
+    gecho ".oh-my-zsh found, ignoring..." >&2
 fi
 
 # Installing futurama quotes
 if [ ! -e ~/.futurama ]; then
-    yecho "[message] ~/.futurama not found, downloading..." >&2
-    command -v gshuf || (yecho "[message] gshuf not found, installing coreutils from Homebrew..." >&2 && brew install coreutils)
+    yecho ".futurama not found, downloading..." >&2
+    command -v gshuf || (yecho "gshuf not found, installing coreutils from Homebrew..." >&2 && brew install coreutils)
     curl -s https://raw.github.com/vsbuffalo/good-news-everyone/master/futurama.txt 2> /dev/null | \
 	awk '{print $0} END{print "total quotes: "NR > "/dev/stderr"}' > ~/.futurama
-else 
-    gecho "[message] ~/.futurama found, ignoring..." >&2
-fi
-
-# Copy over ./zshrc
-if [ ! -e ~/.zshrc -a ! -L ~/.zshrc ]; then
-    yecho "[message] ~/.zshrc not found, linking..." >&2
-    ln -s dotfiles/.zshrc ~/.zshrc
 else
-    gecho "[message] ~/.zshrc found, ignoring..." >&2
+    gecho ".futurama found, ignoring..." >&2
 fi
 
-# Copy over .gitconfig
-if [ ! -e ~/.gitconfig -a ! -L ~/.gitconfig ]; then
-    yecho "[message] ~/.gitconfig not found, linking..." >&2
-    ln -s dotfiles/.gitconfig ~/.gitconfig
-else
-    gecho "[message] ~/.gitconfig found, ignoring..." >&2
-fi
+# Link over .zshrc
+linkdotfile .zshrc
 
-# Copy over .tmux.conf
-if [ ! -e ~/.tmux.conf -a ! -L ~/.tmux.conf ]; then
-    command -v tmux > /dev/null || (yecho "[message] tmux not found, installing coreutils from Homebrew..." >&2 && brew install tmux)
-    ln -s dotfiles/.tmux.conf ~/.tmux.conf
-    yecho "[message] ~/.tmux.conf not found, linking..." >&2
-else
-    gecho "[message] ~/.tmux.conf found, ignoring..." >&2
-fi
+# Link over .gitconfig
+linkdotfile .gitconfig
 
+# Link over .tmux.conf
+linkdotfile .tmux.conf
+
+# Create Python setup
+linkdotfile .pythonrc.py
+
+# Link Vim settings
+linkdotfile .vimrc
+linkdotfile .vim
+
+# Create a Rprofile
+linkdotfile .Rprofile
 
 # Create a global Git ignore file
 if [ ! -e ~/.global_ignore ]; then
-    yecho "[message] ~/.global_ignore not found, curling from Github..." >&2
+    yecho "~/.global_ignore not found, curling from Github..." >&2
     curl https://raw.github.com/github/gitignore/master/Global/Emacs.gitignore \
     https://raw2.github.com/github/gitignore/master/Global/vim.gitignore \
 	https://raw.github.com/github/gitignore/master/Global/OSX.gitignore > ~/.global_ignore 2> /dev/null
     git config --global core.excludesfile ~/.global_ignore && yecho "[message] adding ignore file to Git..." >&2
 else
-    gecho "[message] ~/.global_ignore found, ignoring..." >&2
+    gecho "~/.global_ignore found, ignoring..." >&2
 fi
 
-# Create a Rprofile
-if [ ! -e ~/.Rprofile -a ! -L ~/.Rprofile ]; then
-    yecho "[message] ~/.Rprofile not found, linking..." >&2
-    ln -s dotfiles/.Rprofile ~/.Rprofile
-    yecho "[message] installing basic R and Bioconductor packages..." >&2
-    
-    Rscript -e "install.packages(c('ggplot2', 'plyr', 'reshape')); source('http://bioconductor.org/biocLite.R');  biocLite(c('GenomicRanges', 'ggbio', 'Gviz', 'GenomicFeatures', 'VariantAnnotation'))" || (recho "[error] could not install R packages - is R installed?" && exit 1)
-else
-    gecho "[message] ~/.Rprofile found, ignoring..." >&2
-fi
-
-# Create Python setup
-if [ ! -e ~/.pythonrc.py -a ! -L ~/.pythonrc.py ]; then
-    yecho "[message] ~/.pythonrc.py not found, linking..." >&2    
-    ln -s dotfiles/.pythonrc.py ~/.pythonrc.py
-else
-    gecho "[message] ~/.pythonrc.py found, ignoring..." >&2
-fi
-
-# Link Vim settings
-if [ ! -e ~/.vimrc ]; then
-  yecho "[message] ~/.vimrc not found, linking" >&2
-  ln -s dotfiles/.vimrc ~/.vimrc
-else
-  gecho "[message] ~/.vimrc found, ignoring..." >&2
-fi
-
-if [ ! -e ~/.vim ]; then
-  yecho "[message] ~/.vim directory not found, linking" >&2
-  ln -s dotfiles/.vim ~/.vim
-else
-  gecho "[message] ~/.vim found, ignoring..." >&2
-fi
+# Install some R packages
+gecho "installing basic R and Bioconductor packages..." >&2
+Rscript -e "install.packages(c('ggplot2', 'plyr', 'reshape')); source('http://bioconductor.org/biocLite.R');  biocLite(c('GenomicRanges', 'ggbio', 'Gviz', 'GenomicFeatures', 'VariantAnnotation'))" || recho "[error] could not install R packages - is R installed?"
 
 
