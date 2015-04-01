7d475e74090c62978e54db0915f98f123a97ba20
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2015-04-01 12:36:29 -0700

    refacted R install stuff

--- install_rpkgs.R
+++ install_rpkgs.R
@@ -1,15 +1,15 @@
 CRAN_PKGS <- c('devtools', 'microbenchmark', 'ggplot2', 'plyr', 'dplyr', 'knitr',
                'reshape2', 'rstan')
 
-BIO_PKGS <- c('GenomicRanges', 'ggbio', 'Gviz', 'GenomicFeatures',
+BIOC_PKGS <- c('GenomicRanges', 'ggbio', 'Gviz', 'GenomicFeatures',
               'VariantAnnotation')
 
 
-install.packages(PKGS)
+install.packages(CRAN_PKGS)
 
 # Install Bioconductor Package
 source('http://bioconductor.org/biocLite.R');
-biocLite(BIO_PKGS)
-
+biocLite(BIOC_PKGS)
+biocLite(ask=FALSE)
 
 
