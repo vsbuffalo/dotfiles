e7a6b325a28d238104405f38a11843a5a2875f70
Vince Buffalo <vsbuffalo@gmail.com>
2023-01-31 16:44:14 -0800

    fast ripgrep keymap for telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+
+-- fast live grep 
+vim.keymap.set('n', '<leader>tg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>td', builtin.diagnostics, {})
+
